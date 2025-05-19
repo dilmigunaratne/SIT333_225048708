97e2763b6d14750174e13b9f4bf88b17084199f5
Dilmi Gunaratne <127681242+dilmigunaratne@users.noreply.github.com>
2025-05-20 05:12:47 +1000

    Update TestLoginServiceFunctional.java

--- springws/src/test/java/web/service/TestLoginServiceFunctional.java
+++ springws/src/test/java/web/service/TestLoginServiceFunctional.java
@@ -13,55 +13,92 @@ import static org.junit.Assert.*;
 
 public class TestLoginServiceFunctional {
 
-    private WebDriver driver;
+private WebDriver driver;
 
-    @Before
-    public void setUp() {
-        // Set ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "/Users/dilmigunaratne/Downloads/chromedriver-mac-arm64-2/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("http://localhost:8080/login");
+@Before
+public void setUp() {
+    // Set ChromeDriver path
+    System.setProperty("webdriver.chrome.driver", "/Users/dilmigunaratne/Downloads/chromedriver-mac-arm64-2/chromedriver");
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("http://localhost:8080/login");
+}
+
+@Test
+public void testLoginSuccess() {
+    login("dilmi", "dilmi123", "2002-01-01");
+    assertTrue(driver.getCurrentUrl().contains("/q1"));
+}
+
+@Test
+public void testLoginFailureInvalidCredentials() {
+    login("dilmi", "wrongPass", "2002-01-01");
+    WebElement errorMessage = driver.findElement(By.id("errorMessage"));
+    System.out.println(errorMessage.getText());
+    assertNotNull(errorMessage);
+    assertTrue(errorMessage.getText().contains("Incorrect credentials"));
+}
+
+@Test
+public void testLoginFailureInvalidUsername() {
+    login("wrongUser", "dilmi123", "2002-01-01");
+    WebElement errorMessage = driver.findElement(By.id("errorMessage"));
+    System.out.println(errorMessage.getText());
+    assertNotNull(errorMessage);
+    assertTrue(errorMessage.getText().contains("Incorrect credentials"));
+}
+
+@Test
+public void testLoginFailureEmptyUsername() {
+    login("", "dilmi123", "2002-01-01");
+    WebElement errorMessage = driver.findElement(By.id("errorMessage"));
+    System.out.println(errorMessage.getText());
+    assertNotNull(errorMessage);
+    assertTrue(errorMessage.getText().contains("Username cannot be empty"));
+}
+
+@Test
+public void testLoginFailureEmptyPassword() {
+    login("dilmi", "", "2002-01-01");
+    WebElement errorMessage = driver.findElement(By.id("errorMessage"));
+    System.out.println(errorMessage.getText());
+    assertNotNull(errorMessage);
+    assertTrue(errorMessage.getText().contains("Password cannot be empty"));
+}
+
+@Test
+public void testLoginFailureEmptyDOB() {
+    login("dilmi", "dilmi123", "");
+    WebElement errorMessage = driver.findElement(By.id("errorMessage"));
+    System.out.println(errorMessage.getText());
+    assertNotNull(errorMessage);
+    assertTrue(errorMessage.getText().contains("Date of Birth cannot be empty"));
+}
+
+@Test
+public void testLoginFailureCaseSensitiveUsername() {
+    login("DILMI", "dilmi123", "2002-01-01");
+    WebElement errorMessage = driver.findElement(By.id("errorMessage"));
+    System.out.println(errorMessage.getText());
+    assertNotNull(errorMessage);
+    assertTrue(errorMessage.getText().contains("Incorrect credentials"));
+}
+
+private void login(String username, String password, String dob) {
+    driver.findElement(By.name("username")).sendKeys(username);
+    driver.findElement(By.name("passwd")).sendKeys(password);
+
+    WebElement dobElement = driver.findElement(By.name("dob"));
+    ((JavascriptExecutor) driver).executeScript("arguments[0].value = arguments[1];", dobElement, dob);
+
+    driver.findElement(By.id("submit")).click();
+}
+
+@After
+public void tearDown() {
+    if (driver != null) {
+        driver.quit();
     }
+}
 
-    @Test
-    public void testLoginSuccess() {
-        login("dilmi", "dilmi123", "2002-01-01");
-        assertTrue(driver.getCurrentUrl().contains("/q1"));
-    }
-
-    @Test
-    public void testLoginFailureInvalidCredentials() {
-        login("dilmi", "wrongPass", "2002-01-01");
-        WebElement errorMessage = driver.findElement(By.id("errorMessage"));
-        assertNotNull(errorMessage);
-        assertTrue(errorMessage.getText().contains("Incorrect credentials."));
-    }
-
-    @Test
-    public void testLoginFailureInvalidDOB() {
-        login("dilmi", "dilmi123", "1990-01-01");
-        WebElement errorMessage = driver.findElement(By.id("errorMessage"));
-        assertNotNull(errorMessage);
-        assertTrue(errorMessage.getText().contains("Incorrect credentials."));
-    }
-
-    private void login(String username, String password, String dob) {
-        driver.findElement(By.name("username")).sendKeys(username);
-        driver.findElement(By.name("passwd")).sendKeys(password);
-        
-        // Use JavaScript to set the date input value correctly
-        WebElement dobElement = driver.findElement(By.name("dob"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].value = arguments[1];", dobElement, dob);
-        
-        driver.findElement(By.id("submit")).click();
-    }
-
-
-    @After
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 }
